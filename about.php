1361e14d7515dd8e260cd54d85e11f33af600503
Benjamin Kwame Ampah <58374644+benjamin2310@users.noreply.github.com>
2021-03-29 14:16:31 +0000

    Update about.php

--- about.php
+++ about.php
@@ -60,29 +60,6 @@
                 <div class="clearfix"></div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">Our
-                        <strong>Team</strong>
-                    </h2>
-                    <hr>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="img/abt1.jpg" alt="CEO">
-                    <h3>Benjamin Ampah</h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="img/abt2.jpg" alt="Co-Worker">
-                    <h3>Rebecca Otu</h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="img/abt3.jpg" alt="Administrator">
-                    <h3>Sandra Coffie</h3>
-                </div>
-                <div class="clearfix"></div>
             </div>
         </div>
 
